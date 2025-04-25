6838e0cf2918da8f3ad4c182866c70dad5445e55
thi-coelho <thiago.alvescoelho93@gmail.com>
2025-04-25 15:10:02 +0100

    Creation of ENUM classes
    The ENUM classes were created for: Department / Manager / Menu option in order to create a set of constant values without having to assign each value a number or having to match a String.

---
+++ src/techcompany/ManagerType.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package techcompany;
+
+/**
+ *
+ * @author: 
+ * Thiago Alves Coelho - 2024287
+ */
+public enum ManagerType {
+    HEAD_MANAGER,
+    ASSISTANT_MANAGER,
+    TEAM_LEAD;
+}
---
+++ src/techcompany/MenuOption.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package techcompany;
+
+/**
+ *
+ * @author: 
+ * Thiago Alves Coelho - 2024287
+ */
+public enum MenuOption {
+    SORT,
+    SEARCH,
+    ADD_RECORDS,
+    GENERATE_RANDOM,
+    EXIT;
+}
